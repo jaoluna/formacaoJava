147148f8affca67b00ef2141a020ef12b790e3f0
João Luna <joaovic135@hotmail.com>
2022-01-11 15:12:47 -0300

    Repeticoes e break

---
+++ sintaxe-variaveis-e-fluxo/src/Multiplos3.java
@@ -0,0 +1,9 @@
+public class Multiplos3 {
+    public static void main(String[] args) {
+        for (int multiplo = 0; multiplo <= 100; multiplo++){
+            if (multiplo % 3 == 0){
+                System.out.println(multiplo);
+            }
+        }
+    }
+}
