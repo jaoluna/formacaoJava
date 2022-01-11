8ab2f6ab60d7ab2e271137aa510ad4a2c27e69ef
João Luna <joaovic135@hotmail.com>
2022-01-11 00:07:27 -0300

    aprendendo a usar variaveis

---
+++ sintaxe-variaveis-e-fluxo/src/TestaConversao.java
@@ -0,0 +1,18 @@
+public class TestaConversao {
+    public static void main(String[] args){
+        double salario = 1270.50;
+
+        float pontoFlutuante = 3.14f;
+        int valor = (int) salario;
+        System.out.println(valor);
+
+        long numeroGrande = 1239198378127893L;
+
+        double valor1 = 0.2;
+        double valor2 = 0.1;
+        double total = valor1 + valor2;
+
+        System.out.println(total);
+
+    }
+}
