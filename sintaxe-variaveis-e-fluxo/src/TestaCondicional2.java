82bcb9131f3d15f3c9a9a5769f325f3d4201351c
João Luna <joaovic135@hotmail.com>
2022-01-11 14:41:51 -0300

    controle de fluxo

---
+++ sintaxe-variaveis-e-fluxo/src/TestaCondicional2.java
@@ -0,0 +1,17 @@
+public class TestaCondicional2 {
+    public static void main(String[] args) {
+        System.out.println("testando condicionais");
+        int idade = 20;
+        int quantidadePessoas = 3;
+        boolean acompanhando = quantidadePessoas >= 2;
+
+        System.out.println("valor de acompanhado = " + acompanhando);
+        if (idade >= 18 && acompanhando) {
+            System.out.println("seja bem vindo");
+        } else {
+                System.out.println("infelizmente você não pode entrar");
+        }
+
+
+    }
+}
---
+++ sintaxe-variaveis-e-fluxo/src/TestaValores.java
@@ -0,0 +1,12 @@
+public class TestaValores {
+    public static void main(String[] args) {
+        int primeiroNumero = 5;
+        int segundoNumero = 7;
+        segundoNumero = primeiroNumero;
+        primeiroNumero = 10;
+
+        // quanto vale  o segundoNumero?
+
+        System.out.println(segundoNumero);
+    }
+}
