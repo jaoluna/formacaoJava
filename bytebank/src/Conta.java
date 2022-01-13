7706a1cac509d70baad090b31c94017e6853a655
João Luna <joaovic135@hotmail.com>
2022-01-13 18:06:31 -0300

    criando métodos

--- bytebank/src/Conta.java
+++ bytebank/src/Conta.java
@@ -5,4 +5,24 @@ public class Conta {
     int numero;
     String titular;
 
+    public void deposita(double valor){
+        this.saldo += valor;
+    }
+
+    public boolean saca(double valor){
+        if (this.saldo >= valor){
+            this.saldo -= valor;
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public boolean transfere(double valor, Conta destino){
+        if (this.saca(valor)){
+            destino.deposita(valor);
+            return true;
+        }
+        return false;
+    }
 }
